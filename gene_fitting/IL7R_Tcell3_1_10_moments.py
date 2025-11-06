38205375e21044ab67e42b48182d7882e7c8ebcf
CatherineFelce <109311417+CatherineFelce@users.noreply.github.com>
2025-11-06 14:27:35 -0800

    Update import paths and filter parameters

--- gene_fitting/IL7R_Tcell3_1_10_moments.py
+++ gene_fitting/IL7R_Tcell3_1_10_moments.py
@@ -1,35 +1,27 @@
 import sys
-sys.path.insert(0, '/home/cat/monod/src/monod')
+# Add path to Monod source code.
+sys.path.insert(0, '/home/user/monod/src/monod')
 
 import extract_data, cme_toolbox, inference, analysis
 import importlib
 import numpy as np
-
 import pickle
-
 import logging, sys
+import warnings
+
 logging.basicConfig(stream=sys.stdout)
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
-import warnings
 warnings.filterwarnings("ignore") #warning suppression within script is not respected by colab
 warnings.simplefilter('ignore')
-
-# Reload modules to get latest changes
-importlib.reload(inference)
-importlib.reload(extract_data)
-importlib.reload(analysis)
-importlib.reload(cme_toolbox)
 
 gridsize = [2,2,2]
 samp_lb, samp_ub = [-7, -2, -2.5],[-5, 0, -0.] 
 # poisson average log length is 5.
-# default sample bounds are: "Poisson":{'samp_lb':[-8, -3], 'samp_ub':[-5, 0],'gridsize':[6, 7]}}
 
 # Default physical bounds
 phys_ub, phys_lb = None, None
 
-# filt_param = {'min_means':[0.01]*3, 'max_maxes':[350, 350, 10000], 'min_maxes':[1,1, 1]}
 filt_param = {'min_means':[0]+ [0.01]*2, 'max_maxes':[10000]+[ 350, 10000], 'min_maxes':[0]+ [1,1]}
 
 h5ad_filepath = './combined_adata_tcells_3_with_ambiguous.h5ad'  # Path to save the .h5ad file
